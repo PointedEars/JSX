5516803208ea660009d221cb9e13c5dfe4e35f9d
Thomas 'PointedEars' Lahn <PointedEars@web.de>
2005-09-14 02:00:00 +0000

    git-svn-id: svn://pointedears.de/home/mw3020/f001/svn/JSX/trunk@59 07eaf04e-f8cd-4426-bdba-a888e727c51b

---
+++ regexp.js
@@ -0,0 +1,144 @@
+/** 
+ * <title>PointedEars' JSX: RegExp Library</title>
+ */
+if (typeof RegExp == "undefined")
+{
+  var RegExp = new Object();
+}
+/** @version */ RegExp.version = "0.1.2005091404";
+/**
+ * @filename regexp.js
+ * @partof   PointedEars' JavaScript Extensions (JSX)
+ *
+ * @section Copyright & Disclaimer
+ *
+ * @author
+ *   (C) 2005, 2008  Thomas Lahn &lt;regexp.js@PointedEars.de&gt;
+ */
+RegExp.copyright = "Copyright \xA9 2005";
+RegExp.author    = "Thomas Lahn";
+RegExp.email     = "regexp.js@PointedEars.de";
+RegExp.path      = "http://pointedears.de/scripts/";
+//RegExp.docURL    = RegExp.path + "regexp.htm";
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License (GPL) for more details.
+ *
+ * You should have received a copy of the GNU GPL along with this
+ * program (COPYING file); if not, go to [1] or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA.
+ * 
+ * [1] <http://www.gnu.org/licenses/licenses.html#GPL>
+ */
+// Refer string.htm file for general documentation. 
+
+/**
+ * Returns the string representation of a RegExp object without delimiters.
+ * 
+ * @param rx
+ * @return string
+ */
+function regexp2str(rx)
+{
+//return rx.toString().replace(/[^\/]*\/((\\\/|[^\/])+)\/[^\/]*/, "$1");
+  return rx.source || rx.toString().replace(/[^\/]*\/(.+)\/[^\/]*/, "$1");
+}
+RegExp.prototype.toString2 = regexp2str;
+
+/**
+ * Concatenates strings or Regular Expressions (RegExp)
+ * and returns a reference to the resulting RegExp.
+ * 
+ * If flags are used with either RegExp argument, the
+ * resulting RegExp object has all of those flags set.
+ * 
+ * @author Copyright (c) 2005
+ *   Thomas Lahn &lt;regexp.js@PointedEars.de&gt;
+ * @partof
+ *   http://pointedears.de/scripts/regexp.js
+ * @params
+ *   Expressions to be concatenated.  If a not a RegExp, the
+ *   argument is converted to String and all resulting characters
+ *   except "(", "|" and ")" are used literally; this allows for
+ *   expressions to be grouped and used in alternation.  For
+ *   characters to lose their special meaning, escape them in
+ *   a RegExp argument.
+ * 
+ *   If this function is called as method of a RegExp
+ *   object, the expressions given are concatenated
+ *   beginning with the calling object.
+ * @return RegExp
+ *   A reference to the resulting RegExp.
+ */
+function regexp_concat()
+{
+  var aParts = [], start = 0, c;
+    
+  if ((c = this.constructor) && c == RegExp)
+  {
+    aParts.push(regexp2str(this));
+  }
+
+  var oFlags = {
+    g: false,
+    i: false,
+    m: false,
+    
+    joinSet: function() {
+      var a = [], oDummy = {g: 1, i: 1, m: 1};
+      
+      for (var j in oDummy)
+      {
+        if (this[j] === true)
+        {
+          a.push(j);
+        }
+      }
+      
+      return a.join("");
+    }
+  };
+  
+  for (var i = 0, iArgnum = arguments.length, a; i < iArgnum; i++)
+  {
+    if ((c = (a = arguments[i]).constructor) && c == RegExp)
+    {
+      aParts.push(regexp2str(a));
+      if (!oFlags.g && a.global)
+        oFlags.g = true;
+
+      if (!oFlags.i && a.ignoreCase)
+        oFlags.i = true;
+
+      if (!oFlags.m && a.multiline)
+        oFlags.m = true;
+    }
+    else
+    {
+      aParts.push(String(a).replace(/([^|()])/g, "\\$1"));
+    }
+  }
+
+  return new RegExp(aParts.join(""), oFlags.joinSet());
+}
+RegExp.prototype.concat = regexp_concat;
+
+// test case
+/*
+  alert([
+    /("([^"\\]|\\")*"|'([^'\\]|\\')*')|\/\*([^*]|\*[^\/])*\*\/|\/\/[^\n\r]*([\n\r]|$)|(\/(\\\/|[^\/])+\/)/mg,
+    regexp_concat(
+      '(', /"([^"\\]|\\")*"/, '|', /'([^'\\]|\\')*'/,
+      ')|', /\/\*([^*]|\*[^\/])*\*\//,
+      '|', /\/\/[^\n\r]*([\n\r]|$)/,
+      '|', /(\/(\\\/|[^\/])+\/)/mg)
+  ].join("\n"));
+*/
