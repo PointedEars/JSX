32acf097dbac8a4c6189039fc5d72dbc0145a310
PointedEars <PointedEars@07eaf04e-f8cd-4426-bdba-a888e727c51b>
2005-11-29 02:00:00 +0000

    search.js/1.10/2005-11-29-03: - Added feature test for location property - Fixed and optimized URI decoder - Pretty printing - Fixed JSdoc - Failed SearchString.prototype.getValue() defaults to empty string   (instead of null)

--- search.js
+++ search.js
@@ -7,7 +7,7 @@
  */
 function SearchString(/** @argument string */ s)
 {
-  this.version = "1.09.2004030606";
+  this.version = "1.10.2005112903";
 /**
  * @file search.js
  * @partof PointedEars' JavaScript Extensions (JSX)
@@ -16,10 +16,10 @@ function SearchString(/** @argument string */ s)
  *         Thomas Lahn &lt;search.js@PointedEars.de&gt;
  *
  */
-  this.copyright = "Copyright \xA9 2000-2004";
+  this.copyright = "Copyright \xA9 2000-2005";
   this.author    = "Thomas Lahn";
   this.email     = "search.js@PointedEars.de";
-  this.path      = "http://pointedears.de/scripts/";
+  this.path      = "http://pointedears.de.vu/scripts/";
   this.URI       = this.path + "search.js";
 /**
  * This program is free software; you can redistribute it and/or
@@ -43,7 +43,8 @@ function SearchString(/** @argument string */ s)
 
   if (typeof s != "string")
   {
-    if (location && typeof location.search != "undefined")
+    if (typeof location != "undefined"
+        && typeof location.search != "undefined")
     {
       s = location.search;
     }
@@ -56,24 +57,19 @@ function SearchString(/** @argument string */ s)
   s = s.substring(s.lastIndexOf("?") + 1);
   this.text = s;
 
-  // Determine method to be used for decoding URI components -- b0rken
-  /*
-  var fUnescape =
-    (typeof decodeURIComponent != "undefined"
+  // Determine method to be used for decoding URI components
+  var decode =
+    (isMethodType(typeof decodeURIComponent)
       ? decodeURIComponent
-      : (typeof unescape != "undefined"
+      : (isMethodType(typeof unescape)
           ? unescape
           : dummy));
-  */
  
   /*
    * For Mozilla/NS: Replace occurrences of "&nbsp;" with
    * character "%A0"; requires linked string.js
    */
-  this.text = replaceText(this.text, "&nbsp;", unescape("%A0"));
-  
-  // Replace occurrences of "%xy" with the character
-  this.text = unescape(this.text);
+  this.text = replaceText(this.text, "&nbsp;", "%A0");
   
   this.length = s.length;
 
@@ -89,48 +85,45 @@ function SearchString(/** @argument string */ s)
   if (this.length > 0)
   {
     var value_pair, sName, sValue;
+
     if (isMethodType(typeof this.text.split))
     {
-      var value_pairs = this.text.split("&");
-      var p;
-      for (i = 0; i < value_pairs.length; i++)
+      var value_pairs = this.text.split(new RegExp("[&;]")), len;
+      for (i = 0, len = value_pairs.length; i < len; i++)
       {
-        p = value_pairs[i];
+        var p = value_pairs[i];
         value_pair = p.split("=");
-        sName = value_pair[0];
-        sValue =
-          (value_pair.length > 1
+        sName = decode(value_pair[0]);
+        sValue = (value_pair.length > 1
             ? value_pair[1]
             : (sName = "", value_pair[0]));
 
-        addValue(this.values, sName, sValue)
+        addValue(this.values, sName, decode(sValue))
       }
     }
     else
     {
-      i = 0;
-      
-      var idxEquals;
-      while (i < this.length)
+      for (i = 0; i < this.length;)
       {
         var idxAmp = this.text.substring(i).indexOf("&");
         // Required by law
         (idxAmp < 0) ? (idxAmp = this.length) : (idxAmp += i);
         
         value_pair = this.text.substring(i, idxAmp);
-        idxEquals = value_pair.indexOf("=");
+        var idxEquals = value_pair.indexOf("=");
         sName = "";
-        sValue = "";
 
         if (idxEquals < 0)
+        {
           sValue = value_pair;
+        }
         else                                                      
         {
           sName = value_pair.substring(0, idxEquals);
           sValue = value_pair.substring(idxEquals + 1);
         }
         
-        addValue(this.values, sName, sValue);
+        addValue(this.values, decode(sName), decode(sValue));
         
         i += value_pair.length + 1;
       }
@@ -138,14 +131,15 @@ function SearchString(/** @argument string */ s)
   }
 }
 
-SearchString.prototype.isName = function searchString_isName(
-  /** @argument string                */ sName,
-  /** @optional boolean default false */ bCaseSensitive)
+SearchString.prototype.isName =
 /**
- * @argdescr bCaseSensitive Provide <code>true</code> to ignore name case.
- * @returns <code>true</code> if the name <var>sName</var> exists,
- * <code>false</code> otherwise.
+ * @param sName: string
+ * @param bCaseSensitive: optional boolean = false 
+ *   Provide <code>true</code> for case-sensitivity.
+ * @return <code>true</code> if the name <var>sName</var> exists,
+ *   <code>false</code> otherwise.
  */
+function searchString_isName(sName, bCaseSensitive)
 {
   if (!bCaseSensitive)
   {
@@ -155,48 +149,51 @@ SearchString.prototype.isName = function searchString_isName(
   return (typeof this.values[sName] != "undefined");
 }
 
-SearchString.prototype.hasValue = function searchString_hasValue(
-  /** @argument string                */ sName,
-  /** @optional boolean default false */ bCaseSensitive)
+SearchString.prototype.hasValue =
 /**
- * @argdescr bCaseSensitive Provide <code>true</code> to ignore name case.
- * @returns <code>true</code> if the name <var>sName</var> exists and its
- * value is not the empty string, <code>false</code> otherwise.
+ * @param sName: string
+ * @param bCaseSensitive: optional boolean = false
+ *   Provide <code>true</code> for case-sensitivity.
+ * @return <code>true</code> if the name <var>sName</var> exists and its
+ *   value is not the empty string, <code>false</code> otherwise.
  */
+function searchString_hasValue(sName, bCaseSensitive)
 {
   if (!bCaseSensitive)
   {
     sName = sName.toLowerCase();
   }
 
-  return this.isName(sName, bCaseSensitive) && this.values[sName] != "";
+  return (this.isName(sName, bCaseSensitive) && this.values[sName] != "");
 }
 
-SearchString.prototype.getValue = function searchString_getValue(
-  /** @argument string                */ sName,
-  /** @optional boolean default false */ bCaseSensitive,
-  /** @optional boolean default false */ bConvertCode)
+SearchString.prototype.getValue =
 /**
- * @argdescr bCaseSensitive Provide <code>true</code> for case-sensitivity.
- * @returns the value of the name <var>sName</var> if it exists, "" otherwise.
+ * @param sName: string
+ * @param bCaseSensitive: optional boolean = false
+ *   Provide <code>true</code> for case-sensitivity.
+ * @param bConvertCode: optional boolean = false
+ * @return The value of the name <var>sName</var> if it exists, "" otherwise.
  */
+function searchString_getValue(sName, bCaseSensitive, bConvertCode)
 {
   if (!bCaseSensitive)
   {
     sName = sName.toLowerCase();
   }
 
-  return (this.isName(sName) ? this.values[sName] : null);
+  return (this.isName(sName) ? this.values[sName] : "");
 }
 
-function addValue(
-  /** @argument object */ o,
-  /** @argument string */ sName,
-  /** @optional string */ sValue)
 /**
- * Helper function for adding value-name pairs
+ * Helper function for adding name-value pairs
  * as properties of SearchString.values
+ * 
+ * @param o: object
+ * @param sName: string
+ * @param sValue: optional string
  */
+function addValue(o, sName, sValue)
 {
   // if there is no property of that name create it
   if (typeof o[sName] == "undefined")
