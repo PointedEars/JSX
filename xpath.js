8bbd4bf18d854b6a10cae99d3912490960577fdc
PointedEars <PointedEars@07eaf04e-f8cd-4426-bdba-a888e727c51b>
2011-02-27 14:58:06 +0000

    * httprequest.html   - Moved test case to test/
    * object.js
      - Replaced date-based versioning with SVN keywords
      - Function.prototype.extend(): Catch Object.defineProperties() exceptions
        to work around potential Google V8 (Chromium) bug with
        ValueCollection.extend() and CSSelectorList.extend()
    
    * style.css
      - Restored Inconsolata as first source code font
      
    * xpath.js
      - Fixed: Cannot evaluate() on foreign documents
      - Changed from this.XPathResult to variable declaration (original also
        has DontDelete attribute set)
      - Added support for MSXML XPath XmlNode::selectNodes()
      - Renamed jsx.xpath.getDefaultNSResolver() to avoid confusion with getters
      - jsx.path.createNSResolver() supports documentNode argument to create
        the resolver for a context document instead of the current one
      - Added jsx.xpath.createCustomNSResolver() to allow easy matching of
        elements in the default namespace
      - Added name to jsx.xpath.InvalidImplementationError
      - Clean-up:
        + Replaced pointless constructor/factory with namespace object
        + Moved everything except XPathResult into jsx.xpath namespace
        + Added SVN keywords
        + Fixed e-mail address

--- xpath.js
+++ xpath.js
@@ -1,14 +1,12 @@
 /**
  * <title>PointedEars' XPath Library</title>
- * @partof
- *   PointedEars JavaScript Extensions (JSX)
+ * @file $Id$
+ * @partof PointedEars JavaScript Extensions (JSX)
  * @requires object.js
- * @recommends exception.js
  *
  * @section Copyright & Disclaimer
  *
- * @author
- *   (C) 2008,2009  Thomas Lahn <dhtml.js@PointedEars.de>
+ * @author (C) 2008‒2011  Thomas Lahn <js@PointedEars.de>
  * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -38,7 +36,7 @@
 
 if (typeof XPathResult == "undefined")
 {
-  this.XPathResult = {
+  var XPathResult = {
     ANY_TYPE: 0,
     NUMBER_TYPE: 1,
     STRING_TYPE: 2,
@@ -52,24 +50,47 @@ if (typeof XPathResult == "undefined")
   };
 }
 
-/**
- * @return undefined|XPath
- */
-jsx.XPath = function () {
-  /* if called as a factory */
-  if (this == jsx.global)
-  {
-    return new XPath();
-  }
-};
+if (typeof jsx == "undefined")
+{
+  /**
+   * @namespace
+   */
+  var jsx = {};
+}
 
-jsx.XPath.prototype = {
-  constructor: jsx.XPath,
+/**
+ * @namespace
+ */
+jsx.xpath = {
+  version:   "0.1.$Revision$ ($Date$)",
+  copyright: "Copyright \xA9 2008-2011",
+  author:    "Thomas Lahn",
+  email:     "js@PointedEars.de",
+  path:      "http://PointedEars.de/scripts/",
+  
+  /**
+   * Returns the result of evaluating an XPath expression against a context node
+   * 
+   * @function
+   * @param expression : String
+   *   XPath expression
+   * @param contextNode : Node
+   *   Context node
+   * @param namespaceResolver : Function
+   *   Namespace resolver.  If not provided, a default namespace
+   *   resolver will be created using {@link jsx.xpath#createDefaultNSResolver()}.
+   *   Is ignored if MSXML is used, as MSXML for JScript supports only
+   *   the default namespace resolver.
+   * @param resultType : Number
+   *   Expected XPath result type.  Should be one of the XPathResult.* constants.
+   *   Is ignored if MSXML is used, as MSXML only supports the equivalent of
+   *   {@link XPathResult#ORDERED_NODE_ITERATOR_TYPE}.
+   * @param oResult : XPathResult
+   *   Optional XPathResult.  For backwards compatibility only.
+   * @return Result of the XPath expression
+   * @type number|string|boolean|Array
+   */
   evaluate:
-    /**
-     * @return Result of the XPath expression
-     * @type number|string|boolean|Array|
-     */
     (function () {
       var jsx_object = jsx.object;
       
@@ -77,76 +98,103 @@ jsx.XPath.prototype = {
                         oResult) {
         var result;
                      
-        if (jsx_object.isMethod(document, "evaluate"))
+        if (!contextNode)
         {
-          if (!contextNode){contextNode = document;}
-          
-          result = document.evaluate(expression,
-            contextNode,
-            namespaceResolver || this.getDefaultNSResolver(contextNode),
-            /^[0-9]$/.test(resultType) ? resultType : 0,
-            oResult || null);
-    
-          if (result)
-          {
-            var found = [];
+          contextNode = document;
+          var documentNode = contextNode;
+        }
+        else
+        {
+          documentNode = contextNode.ownerDocument || contextNode;
+        }
+
+        if (jsx_object.isMethod(documentNode, "evaluate"))
+        {
+          var nsResolver = namespaceResolver
+            || this.createDefaultNSResolver(contextNode, documentNode);
+          var iResultType = resultType || 0;
+          var objResult = oResult || null;
+          result = documentNode.evaluate(expression, contextNode, nsResolver,
+            iResultType, objResult);
+        }
+        else if (jsx_object.isMethod(contextNode, "selectNodes"))
+        {
+          result = contextNode.selectNodes(expression);
+          resultType = XPathResult.ORDERED_NODE_ITERATOR_TYPE;
+        }
             
-            switch (result.resultType)
-            {
-              case XPathResult.NUMBER_TYPE:
-                result = typeof result.numberValue != "undefined"
-                        && result.numberValue;
-                break;
-                
-              case XPathResult.STRING_TYPE:
-                result = typeof result.stringValue != "undefined"
-                        && result.stringValue;
-                break;
-                
-              case XPathResult.BOOLEAN_TYPE:
-                result = typeof result.booleanValue != "undefined"
-                        && result.booleanValue;
-                break;
-                
-              case XPathResult.UNORDERED_NODE_ITERATOR_TYPE:
-              case XPathResult.ORDERED_NODE_ITERATOR_TYPE:
-                if (jsx_object.isMethod(result, "iterateNext"))
-                {
-                  while ((res = result.iterateNext()))
-                  {
-                    found.push(res);
-                  }
-                  
-                  result = found;
-                }
-                break;
-                
-              case XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE:
-              case XPathResult.ORDERED_NODE_SNAPSHOT_TYPE:
-                if (typeof result.snapshotLength != "undefined"
-                    && jsx_object.isMethod(result, "snapshotItem"))
-                {
-                  for (var i = 0, len = result.snapshotLength; i < len; i++)
-                  {
-                    found.push(result.snapshotItem(i));
-                  }
-        
-                  result = found;
-                }
-                break;
-                
-              case XPathResult.ANY_UNORDERED_NODE_TYPE:
-              case XPathResult.FIRST_ORDERED_NODE_TYPE:
-                if (typeof result.singleNodeValue != "undefined")
-                {
-                  result = result.singleNodeValue;
-                }
-                break;
+        if (result)
+        {
+          var found = [];
+          
+          switch (typeof result.resultType != "undefined"
+                   ? result.resultType
+                   : resultType)
+          {
+            case XPathResult.NUMBER_TYPE:
+              result = typeof result.numberValue != "undefined"
+                       && result.numberValue;
+              break;
               
-              default:
-                jsx.throwThis("xpath.InvalidImplementationError");
-                result = null;
-            }
+            case XPathResult.STRING_TYPE:
+              result = typeof result.stringValue != "undefined"
+                       && result.stringValue;
+              break;
+              
+            case XPathResult.BOOLEAN_TYPE:
+              result = typeof result.booleanValue != "undefined"
+                       && result.booleanValue;
+              break;
+              
+            case XPathResult.UNORDERED_NODE_ITERATOR_TYPE:
+            case XPathResult.ORDERED_NODE_ITERATOR_TYPE:
+              if (jsx_object.isMethod(result, "iterateNext"))
+              {
+                /* DOM Level 3 XPath: NodeIterator */
+                while ((res = result.iterateNext()))
+                {
+                  found.push(res);
+                }
+                
+                result = found;
+              }
+              else if (typeof result.length != "undefined")
+              {
+                /* MSXML DOM: NodeList */
+                for (var i = 0, len = result.length; i < len; ++i)
+                {
+                  found.push(result[i]);
+                }
+                
+                result = found;
+              }
+              break;
+              
+            case XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE:
+            case XPathResult.ORDERED_NODE_SNAPSHOT_TYPE:
+              if (typeof result.snapshotLength != "undefined"
+                  && jsx_object.isMethod(result, "snapshotItem"))
+              {
+                for (var i = 0, len = result.snapshotLength; i < len; i++)
+                {
+                  found.push(result.snapshotItem(i));
+                }
+      
+                result = found;
+              }
+              break;
+              
+            case XPathResult.ANY_UNORDERED_NODE_TYPE:
+            case XPathResult.FIRST_ORDERED_NODE_TYPE:
+              if (typeof result.singleNodeValue != "undefined")
+              {
+                result = result.singleNodeValue;
+              }
+              break;
+            
+            default:
+              jsx.throwThis("jsx.xpath.InvalidImplementationError");
+              result = null;
           }
         }
     
@@ -154,21 +202,63 @@ jsx.XPath.prototype = {
       };
     }()),
   
-  getDefaultNSResolver:
-    /**
-     * @return NSResolver|null
-     */
-    function(contextNode) {
-      return (jsx.object.isMethod(document, "createNSResolver")
-        ? document.createNSResolver(
-            (contextNode.ownerDocument == null
-              ? contextNode.documentElement
-              : contextNode.ownerDocument.documentElement))
-        : null);
+  /**
+   * Creates and returns a default namespace resolver, as created and
+   * returned by <code>Document::createNSResolver()</code>, if available,
+   * or <code>null</code>.
+   * 
+   * Note that despite its name a default namespace resolver cannot
+   * resolve elements in the default namespace.  Use
+   * {@link jsx.xpath#createCustomNSResolver()} to create such a resolver
+   * instead.
+   * 
+   * @param contextNode : Node
+   *   Context node for which the default namespace resolver should be
+   *   created.
+   * @param documentNode : Document
+   *   Document node, required for calling the <code>createNSResolver()</code>
+   *   method.  If not specified, <code>document</code> is used.
+   * @return NSResolver|null
+   */
+  createDefaultNSResolver: function(contextNode, documentNode) {
+    if (!documentNode)
+    {
+      documentNode = document;
+    }
+    
+    return (jsx.object.isMethod(documentNode, "createNSResolver")
+      ? documentNode.createNSResolver(
+          (contextNode.ownerDocument == null
+            ? contextNode.documentElement
+            : contextNode.ownerDocument.documentElement))
+      : null);
   },
   
-  InvalidImplementationError: (function() {}).extend(jsx.Error)
+  /**
+   * Creates and returns a custom namespace resolver.  This method exists primarily
+   * to facilitate the matching of elements that are in the default namespace,
+   * XPath defines QNames without prefix to match only elements in the null
+   * namespace.
+   * 
+   * @param namespaces : Object
+   *   Namespace declarations.  The property names are the namespace prefixes,
+   *   the property values are the namespace URIs.
+   * @returns {Function} A namespace resolver that can resolve the
+   *   declared namespaces.  A selector in an undeclared namespace is
+   *   considered to be in the no namespace.
+   */
+  createCustomNSResolver: function(namespaces) {
+    return function(prefix) {
+      return namespaces[prefix] || null;
+    };
+  },
+  
+  /**
+   * Exception thrown if an unsupported XPath implementation is encountered
+   * 
+   * @function
+   */
+  InvalidImplementationError: (function() {}).extend(jsx.Error, {
+    name: "jsx.xpath.InvalidImplementationError"
+  })
 };
-
-var xpath = new jsx.XPath();
-jsx.xpath = xpath;
