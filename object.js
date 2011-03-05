8ef3cbfc0b379c49b34f19b9552183b33e4c0a78
Thomas 'PointedEars' Lahn <PointedEars@web.de>
2011-03-05 22:54:08 +0000

    * ani.js   - Adapted for changed Color namespace   - Fixed JSdoc
    * css.js
      - Moved everything to jsx.css namespace
      - Replaced date-based versioning with SVN keywords
    
    * regexp.js
      - Moved everything to jsx.regexp namespace
      - Replaced date-based versioning with SVN keywords
    
    * UnicodeData.js
      - Adapted for new jsx.regexp namespace
    
    * xpath.js
      - Added support for default namespaces
    
    * General
      - Major reorg: moved old documentation to doc/historic/,
        test cases to test/, old SELFHTML.DE JS FAQ to faq/
      - Updated major scripts to GPLv3 (includes auto-format
        like bracing)
    
    git-svn-id: svn://pointedears.de/home/mw3020/f001/svn/JSX/trunk@209 07eaf04e-f8cd-4426-bdba-a888e727c51b

--- object.js
+++ object.js
@@ -2,21 +2,22 @@
  * @fileOverview <title>Basic Object Library</title>
  * @file $Id$
  * 
- * @partof PointedEars' JavaScript Extensions (JSX)
  * @author (C) 2004-2011 <a href="mailto:js@PointedEars.de">Thomas Lahn</a>
  * 
- * This program is free software: you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation, either version 3
- * of the License, or (at your option) any later version.
+ * @partof PointedEars' JavaScript Extensions (JSX)
  * 
- * This program is distributed in the hope that it will be useful,
+ * JSX is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JSX is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with JSX.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /* a more compatible approach */
