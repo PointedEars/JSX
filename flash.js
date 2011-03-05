b1dcb5e938561953ac21ba4d802c5777cf7c8c06
PointedEars <PointedEars@07eaf04e-f8cd-4426-bdba-a888e727c51b>
2011-03-05 22:54:08 +0000

    * ani.js   - Adapted for changed Color namespace   - Fixed JSdoc
    * css.js
      - Moved everything to jsx.css namespace
      - Replaced date-based versioning with SVN keywords
    
    * regexp.js
      - Moved everything to jsx.regexp namespace
      - Replaced date-based versioning with SVN keywords
    
    * UnicodeData.js
      - Adapted for new jsx.regexp namespace
    
    * xpath.js
      - Added support for default namespaces
    
    * General
      - Major reorg: moved old documentation to doc/historic/,
        test cases to test/, old SELFHTML.DE JS FAQ to faq/
      - Updated major scripts to GPLv3 (includes auto-format
        like bracing)

--- flash.js
+++ flash.js
@@ -1,23 +1,24 @@
 /**
  * <title>Flash Support Library</title>
  * @file flash.js
- * @partof PointedEars' JavaScript Extensions (JSX)
  * @requires dhtml.js
  * @author
- *   (C) 2010  Thomas Lahn &lt;js@PointedEars.de&gt;
+ *   (C) 2010, 2011 Thomas Lahn &lt;js@PointedEars.de&gt;
  * 
- * This program is free software: you can redistribute it and/or modify
+ * @partof PointedEars' JavaScript Extensions (JSX)
+ * 
+ * JSX is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
+ *
+ * JSX is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with JSX.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 jsx.dom.flash = {
