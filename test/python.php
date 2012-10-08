7f520dabb80cd1ae0d35d5a0a65b8d395bec592f
Thomas 'PointedEars' Lahn <PointedEars@web.de>
2012-10-08 01:20:49 +0000

    * dom/timeout.js   - jsx.dom.timeout.Timeout.prototype.run():     + Simplified feature test     + Return this for possible chaining   - Updated JSdoc
    * http.js
      - Simplified IDL with WebIDL
      - Simplified setters
      
    * object.js
      - Added Function.prototype.bind() emulation
      - Added Array.prototype.slice() emulation
      - Added Array.from() using Array.protoype.map()
        (simplified list comprehension)
    
    * python.js
      - Fixed aliases
      - Added jsx.python.list.from() (list comprehension)
      - Added jsx.python.range()
    
    * regexp.js
      - Fixed warning texts
      - Fixed pattern-match modifiers
      - Support \d and \s in Unicode mode (TODO: /us case)
    
    * string/hyphenation.js
      - Fixed JSdoc
    
    * string/parser.js
      - Fixed JSdoc
    
    * test/dom/mailto.html
      - General clean-up
      - Added Local Storage demo
    
    * test/python.php
      - Use test.js DOM feature
    
    * test/regexp-test.js
      - Fixed/updated test cases
      - Support asynchronous testing
    
    * test/regexp.php
      - Support asynchronous testing
    
    * test/test.js
      - Support asynchronous testing
    
    * UnicodeData.js
      - Added/resorted escape classes
    
    git-svn-id: svn://pointedears.de/home/mw3020/f001/svn/JSX/trunk@286 07eaf04e-f8cd-4426-bdba-a888e727c51b

--- test/python.php
+++ test/python.php
@@ -12,56 +12,59 @@
     </script>
     <script type="text/javascript" src="../python.js"></script>
     <script type="text/javascript">
-      jsx._import(jsx.python);
-    
-      jsx.test.runner.run({
-        setUp: function(i, f) {
-          jsx.info("Setting up test " + (i + 1) + ":\n" + f);
-        },
-
-        tearDown: function(i, f) {
-          jsx.info("Tearing down test " + (i + 1));
-        },
-
-        tests: [
-          function() {
-            var obj = list();
-            jsx.test.assert(obj.length == 0);
+      function runTests ()
+      {
+        jsx._import(jsx.python);
+      
+        jsx.test.runner.run({
+          setUp: function(i, f) {
+            jsx.info("Setting up test " + (i + 1) + ":\n" + f);
           },
-
-          function() {
-            var obj = list([1]);
-            jsx.test.assert(obj[0] === 1);
+  
+          tearDown: function(i, f) {
+            jsx.info("Tearing down test " + (i + 1));
           },
-
-          function() {
-            var obj = list({foo: "bar"});
-            jsx.test.assert(obj[0] === "foo");
-          },
-
-          function() {
-            var obj = dict();
-            jsx.test.assert(typeof obj.foo == "undefined");
-          },
-
-          function() {
-            var obj = dict(["foo"], ["bar"]);
-            jsx.test.assert(obj.foo === "bar");
-          },
-
-          function() {
-            var obj = dict({foo: "bar"});
-            jsx.test.assert(obj.foo === "bar");
-          }
-        ]
-      });
-
-      jsx._import(jsx.test);
-      console.log(assert, assertFalse);
+  
+          tests: [
+            function() {
+              var obj = list();
+              jsx.test.assert(obj.length == 0);
+            },
+  
+            function() {
+              var obj = list([1]);
+              jsx.test.assert(obj[0] === 1);
+            },
+  
+            function() {
+              var obj = list({foo: "bar"});
+              jsx.test.assert(obj[0] === "foo");
+            },
+  
+            function() {
+              var obj = dict();
+              jsx.test.assert(typeof obj.foo == "undefined");
+            },
+  
+            function() {
+              var obj = dict(["foo"], ["bar"]);
+              jsx.test.assert(obj.foo === "bar");
+            },
+  
+            function() {
+              var obj = dict({foo: "bar"});
+              jsx.test.assert(obj.foo === "bar");
+            }
+          ]
+        });
+  
+        jsx._import(jsx.test);
+        console.log(assert, assertFalse);
+      }
     </script>
   </head>
 
-  <body>
+  <body onload="runTests()">
     <h1><tt>python.js</tt> Test&nbsp;Case</h1>
   </body>
 </html>
