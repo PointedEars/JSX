971342405e4842ad3fff8a167cd489fa43f34816
Thomas 'PointedEars' Lahn <PointedEars@web.de>
2013-02-18 23:18:10 +0000

    * dom/storage.js: Added support for Session Storage
    git-svn-id: svn://pointedears.de/home/mw3020/f001/svn/JSX/trunk@343 07eaf04e-f8cd-4426-bdba-a888e727c51b

--- dom/storage.js
+++ dom/storage.js
@@ -4,7 +4,7 @@
  * @section Copyright & Disclaimer
  *
  * @author
- *   (C) 2012 Thomas Lahn <js@PointedEars.de>
+ *   (C) 2012, 2013 Thomas Lahn <js@PointedEars.de>
  *
  * @partof PointedEars' JavaScript Extensions (JSX)
  * 
@@ -46,6 +46,49 @@ if (typeof jsx.dom.storage == "undefined")
   jsx.dom.storage = {};
 }
 
+/**
+ * @param storageName : string (optional) Storage name ("local" and "session" are supported)
+ * @returns
+ *   <code>true</code> if any or the specified Storage is supported,
+ *   <code>false</code> otherwise.
+ */
+jsx.dom.storage.isSupported = function (storageName) {
+  if (storageName)
+  {
+    return (typeof window[storageName + "Storage"] != "undefined");
+  }
+  
+  var _storage = jsx.dom.storage;
+  return _storage.localStorage.isSupported() || _storage.sessionStorage.isSupported();
+};
+
+/**
+ * Sets an item in a {@link Storage}. Used internally.
+ * 
+ * @param storage : Storage
+ * @param key : string
+ * @param value
+ */
+jsx.dom.storage.setItem = function (storage, key, value) {
+  storage.setItem(key, JSON.stringify(value));
+};
+
+/**
+ * Retrieves an item from a {@link Storage}. Used internally.
+ * 
+ * @param storage : Storage
+ * @param key : string
+ * @return any item value
+ */
+jsx.dom.storage.getItem = function (storage, key) {
+  var value = storage.getItem(key);
+  
+  /*
+   * NOTE: Shortcut evaluation for handling "" only
+   */
+  return value && JSON.parse(value);
+};
+
 if (typeof jsx.dom.storage.localStorage == "undefined")
 {
   /**
@@ -54,15 +97,106 @@ if (typeof jsx.dom.storage.localStorage == "undefined")
   jsx.dom.storage.localStorage = {};
 }
 
-jsx.dom.storage.localStorage.setItem = function (key, value) {
-  window.localStorage.setItem(key, JSON.stringify(value));
-};
-
-jsx.dom.storage.localStorage.getItem = function (key) {
-  var value = window.localStorage.getItem(key);
+/**
+ * @returns
+ *   <code>true</code> if Local Storage is supported,
+ *   <code>false</code> otherwise.
+ * @function
+ */
+jsx.dom.storage.localStorage.isSupported = (function () {
+  var _storage = jsx.dom.storage;
   
-  /*
-   * NOTE: Shortcut evaluation for handling "" only
+  return function () {
+    return _storage.isSupported("local");
+  };
+}());
+
+/**
+ * Sets an item in the Local {@link Storage}
+ *
+ * @function
+ */
+jsx.dom.storage.localStorage.setItem = (function () {
+  var _storage = jsx.dom.storage;
+  
+  /**
+   * @param key : string
+   * @param value
    */
-  return value && JSON.parse(value);
-};
+  return function (key, value) {
+    _storage.setItem(window.localStorage, key, value);
+  };
+}());
+
+/**
+ * Retrieves an item from the Local {@link Storage}
+ *
+ * @function
+ */
+jsx.dom.storage.localStorage.getItem = (function () {
+  var _storage= jsx.dom.storage;
+  
+  /**
+   * @param key : string
+   * @return any item value
+   */
+  return function (key) {
+    return _storage.getItem(window.localStorage, key);
+  };
+}());
+
+if (typeof jsx.dom.storage.sessionStorage == "undefined")
+{
+  /**
+   * @namespace
+   */
+  jsx.dom.storage.sessionStorage = {};
+}
+
+/**
+ * @returns
+ *   <code>true</code> if Session {@link Storage} is supported
+ *   <code>false</code> otherwise.
+ * @function
+ */
+jsx.dom.storage.sessionStorage.isSupported = (function () {
+  var _storage = jsx.dom.storage;
+  
+  return function () {
+    return _storage.isSupported("session");
+  };
+}());
+
+/**
+ * Sets an item in the Session {@link Storage}
+ *
+ * @function
+ */
+jsx.dom.storage.sessionStorage.setItem = (function () {
+  var _storage = jsx.dom.storage;
+  
+  /**
+   * @param key : string
+   * @param value
+   */
+  return function (key, value) {
+    _storage.setItem(window.sessionStorage, key, value);
+  };
+}());
+
+/**
+ * Retrieves an item from the Session {@link Storage}
+ *
+ * @function
+ */
+jsx.dom.storage.sessionStorage.getItem = (function () {
+  var _storage= jsx.dom.storage;
+
+  /**
+   * @param key : string
+   * @return any item value
+   */
+  return function (key) {
+    return _storage.getItem(window.sessionStorage, key);
+  };
+}());
