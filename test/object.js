cbeeb61a7ea827f6c4d600b377cccc80dca403f3
PointedEars <PointedEars@07eaf04e-f8cd-4426-bdba-a888e727c51b>
2013-04-10 00:06:57 +0000

    * object.js, test/object.js   - jsx.object.setProperties(): Fixed unwanted cloning   - Added testcases

--- test/object.js
+++ test/object.js
@@ -167,7 +167,7 @@ function runTests ()
       },
       {
         feature: 'jsx.object._hasOwnProperty(new Foo(), "bar")',
-        desc: "Return <code>false</code>",
+        desc: "Return <code>false</code> (inherited)",
         code: function () {
           function Foo () {}
           Foo.prototype.bar = 42;
@@ -248,14 +248,198 @@ function runTests ()
 
           Object.keys = Object_keys;
 
-          assertTrue(result && result.length === 1 && result[0] == "foo");
+          assertTrue(result && result.length === 1 && result[0] === "foo");
         }
       },
       {
-        feature: 'jsx.object.clone({foo: "bar"})',
-        desc: "Returns the correct value",
+        feature: 'jsx.object.getKeys(new Foo("baz"))',
+        desc: 'Return <code>["baz"]</code>',
         code: function () {
-          assertTrue(jsx.object.clone({foo: "bar"}).foo === "bar");
+          function Foo (propertyName)
+          {
+            this[propertyName] = 42;
+          }
+
+          Foo.prototype.bar = 42;
+
+          var foo = new Foo("baz");
+
+          var result = jsx.object.getKeys(foo);
+          assertTrue(result && result.length === 1 && result[0] === "baz");
+
+          /* Simulate missing Object.keys() */
+          var Object_keys = Object.keys;
+          delete Object.keys;
+
+          result = jsx.object.getKeys(foo);
+
+          Object.keys = Object_keys;
+
+          assertTrue(result && result.length === 1 && result[0] === "baz");
+        }
+      },
+      {
+        feature: 'jsx.object.clone()',
+        desc: "Returns a shallow clone of <code>jsx.object</code>",
+        code: function () {
+          var clone1 = jsx.object.clone();
+          assertTrue(clone1.clone == jsx.object.clone);
+        }
+      },
+      {
+        feature: 'jsx.object.clone({foo: {bar: "baz"}})',
+        desc: "Returns a shallow clone",
+        code: function () {
+          var o2 = {bar: "baz"};
+          var o = {foo: o2};
+
+          var clone1 = jsx.object.clone(o);
+          assertTrue(clone1 != o && clone1.foo == o2);
+        }
+      },
+      {
+        feature: 'jsx.object.clone({foo: {bar: "baz"}}, 0)',
+        desc: "Returns a shallow clone",
+        code: function () {
+          var o2 = {bar: "baz"};
+          var o = {foo: o2};
+
+          var clone1 = jsx.object.clone(o, 0);
+          assertTrue(clone1 != o && clone1.foo == o2);
+
+          var clone2 = jsx.object.clone(0, o);
+          assertTrue(clone2 != o && clone2.foo == o2);
+        }
+      },
+      {
+        feature: 'jsx.object.clone({foo: {bar: "baz"}}, jsx.object.COPY_ENUM)',
+        desc: "Returns a shallow clone",
+        code: function () {
+          var o2 = {bar: "baz"};
+          var o = {foo: o2};
+
+          var clone1 = jsx.object.clone(o, jsx.object.COPY_ENUM);
+          assertTrue(clone1 != o && clone1.foo == o2);
+
+          var clone2 = jsx.object.clone(jsx.object.COPY_ENUM, o);
+          assertTrue(clone2 != o && clone2.foo == o2);
+        }
+      },
+      {
+        feature: 'jsx.object.clone({foo: {bar: "baz"}}, jsx.object.COPY_ENUM_DEEP)',
+        desc: "Returns a deep clone",
+        code: function () {
+          var o2 = {bar: "baz"};
+          var o = {foo: o2};
+
+          var clone1 = jsx.object.clone(o, jsx.object.COPY_ENUM_DEEP);
+          assertTrue(clone1 != o && clone1.foo != o2 && clone1.foo.bar === "baz");
+
+          var clone2 = jsx.object.clone(jsx.object.COPY_ENUM_DEEP, o);
+          assertTrue(clone2 != o && clone2.foo != o2 && clone2.foo.bar === "baz");
+        }
+      },
+      {
+        feature: 'jsx.object.clone({foo: {bar: "baz"}}, jsx.object.COPY_INHERIT)',
+        desc: "Returns a new inheriting object",
+        code: function () {
+          var o2 = {bar: "baz"};
+          var o = {foo: o2};
+
+          var clone1 = jsx.object.clone(o, jsx.object.COPY_INHERIT);
+          assertTrue(clone1 != o && !clone1.hasOwnProperty("foo")
+            && clone1.foo == o2 && clone1.foo.bar === "baz");
+
+          var clone2 = jsx.object.clone(jsx.object.COPY_INHERIT, o);
+          assertTrue(clone2 != o && !clone2.hasOwnProperty("foo")
+            && clone2.foo == o2 && clone2.foo.bar === "baz");
+        }
+      },
+      {
+        feature: 'jsx.object.setProperties()',
+        desc: "Throws <code>TypeError</code>",
+        code: function () {
+          var success = jsx.tryThis(
+            function () {
+              jsx.object.setProperties({});
+              return true;
+            },
+            function (e) {
+              jsx.error(e);
+              return !(e instanceof TypeError);
+            });
+
+          assertFalse(success);
+        }
+      },
+      {
+        feature: 'jsx.object.setProperties({})',
+        desc: "Throws <code>TypeError</code>",
+        code: function () {
+          var success = jsx.tryThis(
+            function () {
+              jsx.object.setProperties({});
+              return true;
+            },
+            function (e) {
+              jsx.error(e);
+              return !(e instanceof TypeError);
+            });
+
+          assertFalse(success);
+        }
+      },
+      {
+        feature: 'jsx.object.setProperties({}, {foo: "bar"})',
+        desc: "Sets property, no cloning",
+        code: function () {
+          var o = {};
+          var o2 = {bar: "baz"};
+          jsx.object.setProperties(o, {foo: o2});
+
+          assertTrue(o.foo && o.foo == o2 && o.foo.bar === "baz");
+        }
+      },
+      {
+        feature: 'jsx.object.setProperties({foo: "bar"}, {foo: "baz"})',
+        desc: "Does not modify existing property",
+        code: function () {
+          var o = {foo: "bar"};
+          jsx.object.setProperties(o, {foo: "baz"});
+
+          assertTrue(o.foo === "bar");
+        }
+      },
+      {
+        feature: 'jsx.object.setProperties({foo: "bar"}, {foo: "baz"}, jsx.object.ADD_OVERWRITE)',
+        desc: "Modifies existing property",
+        code: function () {
+          var o = {foo: "bar"};
+          jsx.object.setProperties(o, {foo: "baz"}, jsx.object.ADD_OVERWRITE);
+
+          assertTrue(o.foo === "baz");
+        }
+      },
+      {
+        feature: 'jsx.object.setProperties({foo: "bar"}, {foo: "baz"}, jsx.object.COPY_ENUM_DEEP)',
+        desc: "Sets property, with cloning",
+        code: function () {
+          var o = {};
+          var o2 = {bar: "baz"};
+          jsx.object.setProperties(o, {foo: o2}, jsx.object.COPY_ENUM_DEEP);
+
+          assertTrue(o.foo && o.foo != o2 && o.foo.bar === "baz");
+        }
+      },
+      {
+        feature: 'jsx.object.setProperties({foo: "bar"}, {foo: "baz"}, jsx.object.ADD_OVERWRITE | jsx.object.COPY_ENUM_DEEP)',
+        desc: "Modifies existing property, with cloning",
+        code: function () {
+          var o = {foo: "bar"};
+          var o2 = {bar: "baz"};
+          jsx.object.setProperties(o, {foo: o2}, jsx.object.ADD_OVERWRITE | jsx.object.COPY_ENUM_DEEP);
+
+          assertTrue(o.foo && o.foo != o2 && o.foo.bar === "baz");
         }
       }
     ]
