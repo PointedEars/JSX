08dc5bfb07dd765464f376c92e3ae23f67e0faf7
Thomas 'PointedEars' Lahn <PointedEars@web.de>
2013-07-12 04:47:09 +0000

    * General   - Moved css.js:jsx.dom.css.*className() to     dom.js:jsx.dom.*className() (not CSS-specific)
    * dom.js
      -  jsx.dom.removeClassName() returns a boolean success value
      -  jsx.dom.*className() support classList property (DOM4)
    
    git-svn-id: svn://pointedears.de/home/mw3020/f001/svn/JSX/trunk@471 07eaf04e-f8cd-4426-bdba-a888e727c51b

--- dom/compat.js
+++ dom/compat.js
@@ -1,3 +1,5 @@
 var dhtml = jsx.dom;
 var getElem = jsx.dom.getElem;
 var setCont = jsx.dom.setCont;
+jsx.dom.css.removeClassName = jsx.dom.removeClassName;
+jsx.dom.css.addClassName = jsx.dom.addClassName;
