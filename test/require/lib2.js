f79fa4aa0d5ac705a215fa7e0d1c2fe80cd28b60
Thomas 'PointedEars' Lahn <PointedEars@web.de>
2016-05-22 04:09:20 +0200

    Added experimental loadable module support (require.js)

---
+++ test/require/lib2.js
@@ -0,0 +1,4 @@
+jsx.define("require:lib2.js", null, function () {
+  jsx.lib2 = {};
+  jsx.info("lib2.js executed.");
+});
---
+++ test/require/lib3.js
@@ -0,0 +1,4 @@
+jsx.define("require:lib3.js", null, function () {
+  jsx.lib3 = {};
+  jsx.info("lib3.js executed.");
+});
