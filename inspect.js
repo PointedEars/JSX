efadeb9ce4797d9e8bd98b083fcd0396d6ad1bae
Thomas 'PointedEars' Lahn <PointedEars@web.de>
2017-09-04 05:15:48 +0200

    Added inspection module

---
+++ inspect.js
@@ -0,0 +1,49 @@
+jsx.define("JSX:inspect", ["JSX:object"], function () {
+  "use strict";
+
+  jsx.inspect = (function () {
+    function _getPrototypeChain (obj)
+    {
+      var
+        chain = [],
+        proto = obj;
+
+      while ((proto = (proto.__proto__ || Object.getPrototypeOf(proto))))
+      {
+        chain.push(proto);
+      }
+
+      return chain;
+    }
+
+    function _getOwnPropertyInfo (obj, propertyNames)
+    {
+      if (!propertyNames)
+      {
+        propertyNames = Object.getOwnPropertyNames(obj);
+      }
+      else if (!Array.isArray(propertyNames))
+      {
+        propertyNames = [propertyNames];
+      }
+
+      return propertyNames.map(function (name) {
+        var value = this[name];
+
+        return Object.assign(Object.create(null), {
+          name: name,
+          value: value,
+          type: typeof value,
+          "class": jsx.object.getClass(value),
+          constructor: value && value.constructor,
+          descriptor: Object.getOwnPropertyDescriptor(this, name)
+        });
+      }, obj);
+    }
+
+    return {
+      getPrototypeChain: _getPrototypeChain,
+      getOwnPropertyInfo: _getOwnPropertyInfo
+    };
+  }());
+});
