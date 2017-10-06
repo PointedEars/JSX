57ff49723c9e2c148e846acbd5c33492a11f2094
Thomas 'PointedEars' Lahn <PointedEars@web.de>
2017-10-06 07:55:43 +0200

    regexp: Bumped copyright

--- regexp.js
+++ regexp.js
@@ -7,7 +7,7 @@
  * @section Copyright & Disclaimer
  *
  * @author
- *   (C) 2005-2016  Thomas Lahn &lt;js@PointedEars.de&gt;
+ *   (C) 2005, 2009-2017  Thomas Lahn &lt;js@PointedEars.de&gt;
  *
  * @partof PointedEars' JavaScript Extensions (JSX)
  *
@@ -1009,7 +1009,7 @@ if (typeof jsx != "object")
        * @memberOf jsx.regexp
        */
       version:   "0.1.$Revision$",
-      copyright: "Copyright \xA9 2005-2013",
+      copyright: "Copyright \xA9 2005, 2009-2017",
       author:    "Thomas Lahn",
       email:     "js@PointedEars.de",
       path:      "http://pointedears.de/scripts/",
